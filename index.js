8ff323a7d7146afb47ccf9504c5d9006dae34bf7
Niharika Alam <niharikaalam1234@gmail.com>
2024-04-09 20:24:16 -0400

    animate footer wave briefly on scroll

--- index.js
+++ index.js
@@ -45,3 +45,34 @@ $(document).ready(function() {
     ]
   });
 });
+
+// Footer wave animates on scroll
+function isWaveInViewport(el) {
+  var rect     = el.getBoundingClientRect(),
+      vWidth   = window.innerWidth || document.documentElement.clientWidth,
+      vHeight  = window.innerHeight || document.documentElement.clientHeight,
+      efp      = function (x, y) { return document.elementFromPoint(x, y) };     
+
+  // Return false if it's not in the viewport
+  if (rect.right < 0 || rect.bottom < 0 || rect.left > vWidth || rect.top > vHeight)
+    return false;
+
+  return (
+    el.contains(efp(rect.left,  rect.top)) || el.contains(efp(rect.right, rect.top)) ||
+    el.contains(efp(rect.right, rect.bottom)) || el.contains(efp(rect.left,  rect.bottom))
+  );
+}
+
+function animateWaveOnScroll() {
+  let footer = document.querySelector('footer');
+  let wave = footer.querySelector('#wave-2');
+
+  if (isWaveInViewport(footer)) {
+    wave.classList.add('animate-once');
+  } else {
+    wave.classList.remove('animate-once');
+  }
+}
+
+window.addEventListener('scroll', animateWaveOnScroll);
+animateWaveOnScroll();
